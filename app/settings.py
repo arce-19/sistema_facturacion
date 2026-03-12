8c451e83f3f03cfbf81792203b58854d16b18d63
jhanely arce <arcejhanely19@gmail.com>
2026-03-11 19:44:48 -0500

    Permitir host de Render en ALLOWED_HOSTS

--- app/settings.py
+++ app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY=config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1",".herokuapp.com"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
