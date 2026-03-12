3ab11fcc2b88c2ed4833b54969439cf8d254c53c
jhanely arce <arcejhanely19@gmail.com>
2026-03-11 20:55:16 -0500

    Unificacion de requisitos y config de estaticos

--- app/settings.py
+++ app/settings.py
@@ -145,3 +145,5 @@ LOGOUT_REDIRECT_URL = '/login/'
 #DATABASES['default'].update(db_from_env)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
